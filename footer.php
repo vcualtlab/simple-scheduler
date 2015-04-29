bb58f24056489c544c3ae91c4c832d42305e68a8
luetkemj <luetkemj@gmail.com>
2015-04-29 16:56:10 -0400

    use the_modified_time() instead of plugin

--- footer.php
+++ footer.php
@@ -19,7 +19,7 @@
 					</nav>
 
 					<p class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo( 'name' ); ?>.</p>
-
+          <p class="last-modified">Last modified: <?php the_modified_time('F j, Y'); ?></p>
 				</div>
 
 			</footer>
