d68bb24968922ac4bd772b0834c134c72f131c6c
luetkemj <luetkemj@gmail.com>
2015-04-29 16:40:12 -0400

    kill author on pages

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
 
 									<h1 class="h2 entry-title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
 									<p class="byline entry-meta vcard">
-                                        				<?php printf( __( 'Posted %1$s by %2$s', 'bonestheme' ),
+                                  <?php printf( __( 'Posted %1$s by %2$s', 'bonestheme' ),
                        								/* the time the post was published */
                        								'<time class="updated entry-time" datetime="' . get_the_time('Y-m-d') . '" itemprop="datePublished">' . get_the_time(get_option('date_format')) . '</time>',
                        								/* the author of the post */
