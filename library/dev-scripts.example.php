76323d5c38514ddfd0fa813ef33f699e42243a99
luetkemj <luetkemj@gmail.com>
2015-04-30 16:10:05 -0400

    update grunt and npm modules

--- library/dev-scripts.example.php
+++ library/dev-scripts.example.php
@@ -1,5 +1,5 @@
 <script src='//localhost:35729/livereload.js'></script> 
 
-<script src='//172.22.218.54:3000/socket.io/socket.io.js'></script>
-<script>var ___socket___ = io.connect('http://172.22.218.54:3000');</script>
-<script src='//172.22.218.54:3001/client/browser-sync-client.0.8.2.js'></script>
+<script type='text/javascript' id="__bs_script__">//<![CDATA[
+    document.write("<script async src='//HOST:3000/browser-sync/browser-sync-client.1.9.2.js'><\/script>".replace(/HOST/g, location.hostname).replace(/PORT/g, location.port));
+//]]></script>
