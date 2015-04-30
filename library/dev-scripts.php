94a9e0fd0bd53b1e9eb9b9663fb0616101b6106e
luetkemj <luetkemj@gmail.com>
2015-04-30 16:11:06 -0400

    removed dev example file cause we don't need that noise anymore :)

--- library/dev-scripts.php
+++ library/dev-scripts.php
@@ -1,4 +1,4 @@
-<script src='//localhost:35729/livereload.js'></script> 
+<script src="//localhost:35729/livereload.js"></script>
 
 <script type='text/javascript' id="__bs_script__">//<![CDATA[
     document.write("<script async src='//HOST:3000/browser-sync/browser-sync-client.1.9.2.js'><\/script>".replace(/HOST/g, location.hostname).replace(/PORT/g, location.port));
